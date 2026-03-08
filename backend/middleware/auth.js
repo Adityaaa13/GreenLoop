43f2c867034ac63dee87e15ceb33697e08c41735
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2026-03-08 12:07:48 +0530

    changes in Auth Middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const auth = (req, res, next) => {
     const header = req.headers.authorization;
 
     if (!header || !header.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "No token, authorization denied" });
+        return res.status(401).json({ message: "No token, authorization denied,please try again" });
     }
 
     try {
@@ -13,7 +13,7 @@ const auth = (req, res, next) => {
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({ message: "Token is not valid" });
+        return res.status(401).json({ message: "Token is not valid,please try again" });
     }
 };
 
